2d7ba54cf84a15cad0b5e419ddd77d57cffd0f35
max.schneider <max.schneider@student.hpi.uni-potsdam.de>
2018-10-27 22:27:19 +0200

    added test for new author form

--- spec/features/author/new_author_spec.rb
+++ spec/features/author/new_author_spec.rb
@@ -5,4 +5,12 @@ describe "New author page", type: :feature do
   it "should render withour error" do
     visit new_author_path
   end
+
+  it "should display author input fields" do
+      visit new_author_path
+
+      expect(page).to have_field('author[first_name]')
+      expect(page).to have_field('author[last_name]')
+      expact(page).to have_field('author[homepage]')
+  end
 end

035365cf1ac98303c019cb73198458aa5ec1a2ba
max.schneider <max.schneider@student.hpi.uni-potsdam.de>
2018-10-27 22:59:36 +0200

    added model test for author

---
+++ spec/models/author_spec.rb
@@ -0,0 +1,12 @@
+require 'rails_helper'
+
+describe 'author_model', type: :model do
+
+  it "should have a first and second name and an homepage" do
+    author = Author.new(first_name: 'Alan', last_name: 'Turing', homepage: 'http://wikipedia.org/Alan_Turing')
+    expect(author.first_name).to eq('Alan')
+    expect(author.last_name).to eq('Touring')
+    expect(author.homepage).to eq('http://wikipedia.org/Alan_Turing')
+  end
+  
+end

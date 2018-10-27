f7a470693b9356e874e314acdaa7919c9d310e9d
max.schneider <max.schneider@student.hpi.uni-potsdam.de>
2018-10-27 22:47:23 +0200

    added form

--- spec/features/author/new_author_spec.rb
+++ spec/features/author/new_author_spec.rb
@@ -11,6 +11,6 @@ describe "New author page", type: :feature do
 
       expect(page).to have_field('author[first_name]')
       expect(page).to have_field('author[last_name]')
-      expact(page).to have_field('author[homepage]')
+      expect(page).to have_field('author[homepage]')
   end
 end

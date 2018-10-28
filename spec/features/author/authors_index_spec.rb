ded0a8f373826a1a836c369c58665b8e690385dd
max.schneider <max.schneider@student.hpi.uni-potsdam.de>
2018-10-28 15:33:20 +0100

    author index page uses atable to show all author data

--- spec/features/author/authors_index_spec.rb
+++ spec/features/author/authors_index_spec.rb
@@ -2,11 +2,16 @@ require 'rails_helper'
 
 describe "author index page" do
   it "should render withput an error" do
+    visit authors_path
+  end
+
+  it "should have a new button and a author table" do
     @alan=FactoryBot.create :author
     visit authors_path
 
     expect(page).to have_link 'neuen Autor erstellen', href: new_author_path
     expect(page).to have_link @alan.name, href: author_path(@alan)
+    expect(page).to have_link @alan.homepage, href: @alan.homepage
   end
 
 end

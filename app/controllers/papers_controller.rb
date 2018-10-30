2cf8f1bd5cdb7857387ab3ccf44a669af6eb4d54
max.schneider <max.schneider@student.hpi.uni-potsdam.de>
2018-10-30 23:16:02 +0100

    added year filter

--- app/controllers/papers_controller.rb
+++ app/controllers/papers_controller.rb
@@ -3,7 +3,7 @@ class PapersController < ApplicationController
 
   # GET /papers
   def index
-    @papers = Paper.all
+    @papers = Paper.held_at(params[:year])
   end
 
   # GET /papers/1
--- app/models/paper.rb
+++ app/models/paper.rb
@@ -1,5 +1,8 @@
 class Paper < ApplicationRecord
   has_and_belongs_to_many :authors
+
+  scope :held_at, ->(param_year) { where("year == ?", param_year) if param_year.present? }
+
   validates :title, :venue, :year, presence: true
   validates :year, numericality: { only_integer: true }
 end

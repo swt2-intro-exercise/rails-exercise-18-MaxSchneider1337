ed7203c035d47615fe48b8e17f011b6d77b8b831
max.schneider <max.schneider@student.hpi.uni-potsdam.de>
2018-10-30 22:46:02 +0100

    added adding of authors in paper edit

--- app/controllers/papers_controller.rb
+++ app/controllers/papers_controller.rb
@@ -55,6 +55,6 @@ class PapersController < ApplicationController
 
     # Only allow a trusted parameter "white list" through.
     def paper_params
-      params.require(:paper).permit(:title, :venue, :year)
+      params.require(:paper).permit(:title, :venue, :year, :author_ids => [])
     end
 end

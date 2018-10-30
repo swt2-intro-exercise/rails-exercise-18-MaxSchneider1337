de383bf78e91aee0209156bd99c7f86c6f6fcdd2
max.schneider <max.schneider@student.hpi.uni-potsdam.de>
2018-10-30 21:56:56 +0100

    added n:m author paper connection

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,6 +1,7 @@
 class Author < ApplicationRecord
+  has_and_belongs_to_many :papers
   validates :last_name, presence: true
-  
+
   def name
     "#{self.first_name} #{self.last_name}"
   end
--- app/models/paper.rb
+++ app/models/paper.rb
@@ -1,4 +1,5 @@
 class Paper < ApplicationRecord
+  has_and_belongs_to_many :authors
   validates :title, :venue, :year, presence: true
   validates :year, numericality: { only_integer: true }
 end

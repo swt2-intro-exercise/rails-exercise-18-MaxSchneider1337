5f538f2e1ff903f0d53331c1dc6bfa86b6360a32
max.schneider <max.schneider@student.hpi.uni-potsdam.de>
2018-10-30 16:24:28 +0100

    author index shows edit path

--- spec/features/author/authors_index_spec.rb
+++ spec/features/author/authors_index_spec.rb
@@ -12,6 +12,7 @@ describe "author index page" do
     expect(page).to have_link 'neuen Autor erstellen', href: new_author_path
     expect(page).to have_link @alan.name, href: author_path(@alan)
     expect(page).to have_link @alan.homepage, href: @alan.homepage
+    expect(page).to have_link 'edit', href: edit_author_path(@alan)
   end
 
 end
